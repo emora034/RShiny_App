0efff5207bc7942b5e1a27035a8b0aab7790f69b
emora034 <emora034@fiu.edu>
2020-03-11 13:39:38 +0100

    Plot selector init.

--- Energy_app.R
+++ Energy_app.R
@@ -50,8 +50,8 @@ datainfo<-tabPanel("About",
                    mainPanel(style = "font-size:20px", #font-family: 'Lobster', cursive",
                             # p{style = "font-family: Lucida Grande,Lucida Sans Unicode"}
                             p(),
-                     p(helpText("This page contains two datasets regarding the recorded generated renewable energy and the estimated)
-                       capacity for it.")),
+                     p(helpText("This app contains two datasets regarding the recorded generated renewable energy and the estimated
+                       capacity for it.", style = "font-family: Lucida Grande,Lucida Sans Unicode")),
                      p(helpText("The first dataset estimates the installed capacity for renewable energy generation in Maryland, in 
                        megawatts (MW). Reported data comes from energy generators in Maryland registered to generate 
                                 renewable energy credits (RECs) through the PJM Environmental Information Services (EIS) 
@@ -63,7 +63,7 @@ datainfo<-tabPanel("About",
                      p("As renewable energy generators are not required to register in GATS, there may be some renewable energy 
                        generation capacity installed in Maryland but not generating RECs that is not captured in this estimate.",
                        style = "font-family: Lucida Grande,Lucida Sans Unicode"),
-                     p("This data set describes the amount of energy generated annually by renewable sources 
+                     p("The second data set describes the amount of energy generated annually by renewable sources 
                        in Maryland in megawatt hours (MWh). In addition, there is a column which describes 
                        the percent of all energy generated in Maryland coming from renewable sources each year.", style = "font-family: Lucida Grande,Lucida Sans Unicode"),
                      p("Renewable energy generation data comes from PJM's Generation Attribute Tracking System 
@@ -93,13 +93,30 @@ dataPanel<-tabPanel("Data",
 
 
 plotPanel<-tabPanel("Plots",
+        sidebarPanel(
+          selectInput(
+            p(),
+            inputId = "dataset",
+            label="Select a dataset",
+            choices= c(" ", "Renewable Energy Generation Capacity (2006-2017) ", 
+                       "Renewable Energy Generated (2007-2017) "))
+          ,
+          selectInput("variable", "Variable:", choices=NULL),
+          selectInput("year", "Year:", choices=NULL),
+          selectInput("plot.type", "Plot Type:",
+                      list(boxplot="boxplot", histogram="histogram", 
+                           density="density", bar="bar")),
+          checkboxInput("show.points", "show points", TRUE),
+          
                     mainPanel(
-                      p("", style = "font-family: Lucida Grande,Lucida Sans Unicode"),
-                      p("", style = "font-family: Lucida Grande,Lucida Sans Unicode"),
-                    metricsgraphicsOutput("plotdata")
+                      h3(textOutput("caption")),
+                     # p("", style = "font-family: Lucida Grande,Lucida Sans Unicode"),
+                    #  p("", style = "font-family: Lucida Grande,Lucida Sans Unicode"),
+                      uiOutput("plot")
+                    #metricsgraphicsOutput("plotdata")
                       )
 )
-
+)
 ###########################
 ####### USER SERVER #######
 ui<-fluidPage(
@@ -135,12 +152,45 @@ server<-function(input,output){
   
   output$dataTable<-renderDataTable(datasetInput())
                                          
-                                
-  output$plotdata<-renderMetricsgraphics({
-    Leukemia%>%
-      mjs_plot(x=Time, y=Age, width=500, height=800)%>%
-      mjs_point(color_accessor = Status, size_accessor = Age)%>%
-      mjs_labs(x="Survival Time in Months", y="Patients Age")
+  output$caption<-renderText({
+    switch(input$plot.type,
+           "boxplot"="Boxplot",
+           "histogram"="Histogram",
+           "density" ="Density Plot",
+           "bar"="Bar Graph")
+  })
+  output$plot<-renderUI({
+    plotOutput("p")
+  })
+  
+  datasetplot<-reactive({
+    check<-function(x){is.null(x) || x==""}
+    if(check(input$dataset))return()
+    
+    obj<-list(data=get(input$dataset),
+              variable=input$variable,
+              group=input$group)
+    
+    if(any(sapply(obj,check)))return()
+    check<-function(obj){
+      !all(c(obj$variable, obj$group)%in%colnames(obj$data))
+    }
+    if(check(obj))return()
+    obj
+  })
+  
+  output$p<-renderPlot({
+    plot.obj<-get_data()
+    if(is.null(plot.obj))return()
+    
+    if(plot.obj$variable==""|plot.obj$group=="")return()
+    
+    plot.type<-switch(input$plot.type,
+                      "boxplot"=geom_boxplot(),
+                      "histogram"=geom_histogram(),
+                      "density"=geom_density(),
+                      "bar"=geom_bar())
+    
   })
 }
 
